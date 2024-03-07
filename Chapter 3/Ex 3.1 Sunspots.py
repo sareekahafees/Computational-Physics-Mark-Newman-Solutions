42a7bb0da6a8026a4f9aede4fbab00baa98e2800
sharikahafeez <162471591+sharikahafeez@users.noreply.github.com>
2024-03-07 23:33:31 +0500

    Create Ex 3.1 Sunspots.py

---
+++ Chapter 3/Ex 3.1 Sunspots.py
@@ -0,0 +1,20 @@
+#exercise 3.1: plotting experimental data
+#write a program that reads in the sunspot data and makes a graph of sunspots
+
+import numpy as np
+import pylab as plt
+
+data = np.loadtxt('sunspots.txt', float)
+x = data[:1001,0]
+y = data[:1001,1]
+
+for r in range(-5,5,1):
+    yk = y + r
+    Yk = (1/(2*r+1))*yk
+
+plt.plot(x,y)
+plt.plot(x,Yk)
+plt.xlabel('x = month')
+plt.ylabel('y = no. of sunspots')
+plt.xlim(0,1000)
+plt.show()
